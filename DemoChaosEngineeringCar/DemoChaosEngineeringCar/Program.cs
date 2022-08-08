0eb4671f3965ea1086ccb64c691dda21fa05a6c9
Luciano Reichert Eger <lucianoreichert95@hotmail.com>
2022-08-08 19:35:23 -0300

    Update Program.cs

--- DemoChaosEngineeringCar/DemoChaosEngineeringCar/Program.cs
+++ DemoChaosEngineeringCar/DemoChaosEngineeringCar/Program.cs
@@ -11,15 +11,8 @@ builder.Services.AddScoped<CarRepository>();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
