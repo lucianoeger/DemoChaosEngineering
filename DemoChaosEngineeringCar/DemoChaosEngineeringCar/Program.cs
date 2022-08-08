974c04d9dde9eccc55bed09345a0b8dc2b2a7ae3
Luciano Reichert Eger <luciano.eger@ambevtech.com.br>
2022-08-08 19:59:43 -0300

    Migrate automático do DB

--- DemoChaosEngineeringCar/DemoChaosEngineeringCar/Program.cs
+++ DemoChaosEngineeringCar/DemoChaosEngineeringCar/Program.cs
@@ -1,5 +1,6 @@
 using DemoChaosEngineeringCar.Database;
 using DemoChaosEngineeringCar.Repository;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +12,11 @@ builder.Services.AddScoped<CarRepository>();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
+
+using var scope = app.Services.CreateScope();
+using var db = scope.ServiceProvider.GetService<DatabaseContext>();
+db.Database.Migrate();
+
 app.UseSwagger();
 app.UseSwaggerUI();
 app.UseAuthorization();
